38451032ec2a03e6bc81958e273d5670a3012e98
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-02-13 19:43:08 +0530

    [Jyoti Rao] Find frequency of words in a sentence.

---
+++ HashTablesProblems/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HashTablesProblems
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            Console.WriteLine("Welcome to the Stack and Queues Problems");
+            bool check = true;
+            while (check)
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose any options");
+                Console.WriteLine("1.Find Frequency Of words in a sentence \n" +
+                    "12.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        string paragraph = "To be or not to be";
+                        CountNumbOfOccurence(paragraph);
+                        Console.Write("\nPress any key to continue...... ");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Select only valid options");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+        public static void CountNumbOfOccurence(string paragraph)
+        {
+            MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (hashTabe.Exists(word.ToLower()))
+                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                else
+                    hashTabe.Add(word.ToLower(), 1); //to,1 
+            }
+            Console.WriteLine("Displaying after add operation\n---------------------------------");
+            Console.WriteLine("Word      NumberOfFrequency");
+            hashTabe.Display();
+        }
+    }
+}
